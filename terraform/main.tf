7abd19907d270f5b035bbbe1f3b54e700c716544
9868-08 <->
2020-01-15 13:11:51 +0400

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,11 +3,25 @@ terraform {
   required_version = "0.12.19"
 }
 
+provider "google" {
+  # Версия провайдера
+  version = "2.15"
+  # ID проекта
+  project = var.project
+  region  = var.region
+}
+
+# create VPC
+resource "google_compute_network" "vpc" {
+  name                    = "reddit-vpc"
+  auto_create_subnetworks = "false"
+  routing_mode            = "GLOBAL"
+}
 
 resource "google_compute_instance" "app" {
   name         = "reddit-app"
   machine_type = "g1-small"
-#  zone         = "europe-west1"
+  zone         = "europe-north1-a"
   tags         = ["reddit-app"]
   boot_disk {
     initialize_params {
@@ -21,7 +35,7 @@ resource "google_compute_instance" "app" {
   }
 
   metadata = {
-  ssh-keys = "appuser:${file(var.public_key_path)}\nappuser1:${file(var.public_key_path)}\nappuser_web:${file(var.public_key_path)}" }
+  ssh-keys = "appuser:${file(var.public_key_path)}\nappuser1:${file(var.public_key_path)}\n}" }
 
   connection {
     type  = "ssh"
@@ -40,38 +54,17 @@ resource "google_compute_instance" "app" {
   }
 }
 
-#resource "google_compute_firewall" "firewall_puma" {
-#  name = "allow-puma-default"
-#  # Название сети, в которой действует правило
-#  network = "default"
-#  # Какой доступ разрешить
-#  allow {
-#    protocol = "tcp"
-#    ports    = ["9292"]
-#  }
-#  # Каким адресам разрешаем доступ
-#  source_ranges = ["0.0.0.0/0"]
-#  # Правило применимо для инстансов с перечисленными тэгами
-#  target_tags = ["reddit-app"]
-#}
-# setup the GCP provider | provider.tf
-
-terraform {
-  required_version = ">= 0.12"
-}
-
-provider "google" {
-#  credentials = file(var.gcp_auth_file)
-  zone    = var.gcp_zone_1
-  # ID проекта
-  project = var.project
-  region  = var.region
-}
-
-
-provider "google-beta" {
-  project = var.project
-#  credentials = file(var.gcp_auth_file)
-  region  = var.gcp_region_1
-  zone    = var.gcp_zone_1
+resource "google_compute_firewall" "firewall_puma" {
+  name = "allow-puma-default"
+  # Название сети, в которой действует правило
+  network = "default"
+  # Какой доступ разрешить
+  allow {
+    protocol = "tcp"
+    ports    = ["9292"]
+  }
+  # Каким адресам разрешаем доступ
+  source_ranges = ["0.0.0.0/0"]
+  # Правило применимо для инстансов с перечисленными тэгами
+  target_tags = ["reddit-app"]
 }
